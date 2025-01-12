81b28ad9b9234807bd0bc82f7e9d9d82dd646773
aniketyadav16 <142871912+aniketyadav16@users.noreply.github.com>
2025-01-12 11:22:07 +0530

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -1,24 +1,12 @@
 import streamlit as st
 import pickle
 import numpy as np
-import os
 
-model_path1 = os.path.join(os.getcwd(), 'linear_model.pkl')
-model_path2 = os.path.join(os.getcwd(), 'logistic_model.pkl')
 
-if os.path.exists(model_path1):
-    with open(model_path1, 'rb') as f:
-        linear_model = pickle.load(f)
-        print("Linear Model Loaded Successfully!")
-else:
-    print("Error: Linear model file not found.")
+from joblib import load
+linear_mod = load('linear_model.joblib')
+logistic_mod = load('logistic_model.joblib')
 
-if os.path.exists(model_path2):
-    with open(model_path2, 'rb') as f:
-        logistic_model = pickle.load(f)
-        print("Logistic Model Loaded Successfully!")
-else:
-    print("Error: Logistic model file not found.")
 
 
 # Inputs
@@ -40,13 +28,13 @@ if st.button("Predict"):
     input_data = np.array([[feature_1, feature_5, feature_2, feature_3, feature_4]])
     
     # Linear regression prediction
-    linear_pred = linear_model.predict(input_data)
+    linear_pred = linear_mod.predict(input_data)
     st.subheader(f"Predicted Price: {linear_pred[0].round()}")
 
 if st.button("Recommendation"):
     price = st.number_input("Enter Price:")
     input_data2 = np.array([[price, feature_1, feature_5, feature_2, feature_3, feature_4]])
-    logistic_pred = logistic_model.predict(input_data2)
+    logistic_pred = logistic_mod.predict(input_data2)
     if logistic_pred == [1]:
         logistic_pred = "It is Recommended To BUY this DIAMOND!!"
     else:

36a0708301f04d52f0d52a1e53589331bab597f6
aniketyadav16 <142871912+aniketyadav16@users.noreply.github.com>
2025-01-14 19:06:39 +0530

    Update Poly_mod.py

--- Poly_mod.py
+++ Poly_mod.py
@@ -21,7 +21,7 @@ if feature_2 == 0 and feature_3 == 0:
 else:
     feature_5 = ((feature_4 / ((feature_2 + feature_3) / 2))*100) 
 
-st.write(f"feature 5:{feature_5"})
+st.write(f"feature 5:{feature_5}")
 
 if st.button("Predict Price"):
     if feature_5 != 0:

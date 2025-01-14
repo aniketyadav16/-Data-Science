0031691703025026a2645a84b3a4e3939259de10
aniketyadav16 <142871912+aniketyadav16@users.noreply.github.com>
2025-01-14 17:56:04 +0530

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -3,15 +3,23 @@ import pickle
 import numpy as np
 import joblib 
 import requests
+from sklearn.preprocessing import PolynomialFeatures
 
-url = 
+url = "https://github.com/aniketyadav16/-Data-Science/blob/main/Model/polynomial_model.joblib"
+url2 = "https://github.com/aniketyadav16/-Data-Science/blob/main/Model/logistic_model.joblib"
 
+response = requests.get(url)
+with open("poly_model.joblib", "wb") as f:
+    f.write(response.content)
 
+response2 = requests.get(url2)
+with open("log_model.joblib", "wb") as f:
+    f.write(response2.content)
+    
+linear_mod = joblib.load('poly_model.joblib')
+logistic_mod = joblib.load('log_model.joblib')
 
-
-
-linear_mod = load('linear_model.joblib')
-logistic_mod = load('logistic_model.joblib')
+poly = PolynomialFeatures(degree=4)
 
 st.header("Input Features")
 feature_1 = st.number_input("Enter Carat")
@@ -26,14 +34,11 @@ if feature_2 == 0 and feature_3 == 0:
 else:
     feature_5 = feature_4 / ((feature_2 + feature_3) / 2) if (feature_2 + feature_3) != 0 else 0
 
-st.write(f"Feature 5 (Depth Ratio): {feature_5}")
-
-if feature_5 == 0:
-    st.warning("Feature 5 is invalid. Please check your inputs.")
 
 if st.button("Predict"):
     if feature_5 != 0:
         input_data = np.array([[feature_1, feature_5, feature_2, feature_3, feature_4]])
+        input_data = poly.fit_transform(input_data)
         linear_pred = linear_mod.predict(input_data)
         st.subheader(f"Predicted Price: ${linear_pred[0].round()}k")
     else:

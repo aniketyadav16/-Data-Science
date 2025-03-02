a596d7bed8bc2ce5857a0dd9e6ade91bacd7232f
aniketyadav16 <142871912+aniketyadav16@users.noreply.github.com>
2025-03-02 19:30:39 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -91,6 +91,7 @@ with col2:
     st.plotly_chart(fig8)
 
 with col3:
+    st.subheader("3D wave Motion")
     t = np.linspace(0, 4*np.pi, 100)
     x = np.sin(t)
     y = np.cos(t)
@@ -99,7 +100,7 @@ with col3:
     fig223 = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z, mode='lines',
                                        line=dict(width=4, color='cyan'))])
     
-    fig223.update_layout(title="3D Wave Motion", width=900, height=700, template="plotly_dark", title_x=0.5)
+    fig223.update_layout(width=900, height=700, template="plotly_dark", title_x=0.5)
     
     st.plotly_chart(fig223)
 
